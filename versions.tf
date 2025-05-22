f226973bf47fe581c867ce14f7fe56a8c38b0193
Jörn Reder <jreder@dimedis.de>
2025-05-22 13:36:36 +0200

    [versions] feat: bump hetznercloud to 1.50.1

--- versions.tf
+++ versions.tf
@@ -7,7 +7,7 @@ terraform {
     }
     hcloud = {
       source  = "hetznercloud/hcloud"
-      version = ">= 1.49.1"
+      version = ">= 1.50.1"
     }
     local = {
       source  = "hashicorp/local"
